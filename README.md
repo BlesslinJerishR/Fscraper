e41839e6dd50eeb2d760bb4542b69c58b35b543d
Mastermindx33 <mastermindx33@gmail.com>
2021-08-28 13:40:06 +0530

    __build__

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Fscraper
-Automation Scraping usernames of other Instagram users using Fscraper and save it as a Text File for further usage .
+# instagram-follower-scraper
+This python script can automatically scrape other Instagram users' followers and save them in a text file.
+
+# How to use:
+
+1. Install requirements using terminal or command line.
+```sh
+pip install -r requirements.txt
+```
+
+2. Open `fscraper.py` using a text editor and replace the 13th and 14th lines with your Instagram username and password.
+
+3. Open a terminal or cmd again and run the bot using this command: 
+```sh
+python3 fscraper.py
+```
+
+4. Enter the username of the person whose followers you want to scrape.
+
+5. Enter how many followers you want to scrape.
+
+6. The list of usernames in a text file will be saved automatically in the bot folder.
+
+#### Developer : Mastermindx33
---
+++ fscraper.py
@@ -0,0 +1,117 @@
+import os
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager as CM
+
+# Complete these 2 fields ==================
+USERNAME = 'Your Username'
+PASSWORD = 'Your Password'
+# ==========================================
+
+TIMEOUT = 15
+
+def banner():
+    print(f"""
+     _______ #      #       #       #       #       #       #       #
+    (_______)#      #       #       #       #       #       #       #
+     _____   #  ___ #  ____ #  ____ # _____ # ____  # _____ #  ____ #
+    |  ___)  # /___)# / ___)# / ___)#(____ |#|  _ \ #| ___ |# / ___)#
+    | |      #|___ |#( (___ #| |    #/ ___ |#| |_| |#| ____|#| |    #
+    |_|      #(___/ # \____)#|_|    #\_____|#|  __/ #|_____)#|_|    #
+             ##      ##       ##       ##       ##|_|    ##       ##       ##
+
+                         [ Fscraper v.1.2 ]
+                    [ Developer : Mastermindx33 ]
+    """)
+
+def scrape():
+    usr = input('[Required] - Whose followers do you want to scrape: ')
+
+    user_input = int(
+        input('[Required] - How many followers do you want to scrape (60-500 recommended): '))
+
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--log-level=3")
+    mobile_emulation = {
+        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
+    p = os.getcwd()
+    bot = webdriver.Chrome(executable_path= p + "/chromedriver.exe", options=options)
+    bot.set_window_size(600, 1000)
+
+    bot.get('https://www.instagram.com/accounts/login/')
+
+    time.sleep(2)
+
+    print("[Info] - Logging in...")
+
+    user_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]/div/label/input')))
+
+    user_element.send_keys(USERNAME)
+
+    pass_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[4]/div/label/input')))
+
+    pass_element.send_keys(PASSWORD)
+
+    login_button = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="loginForm"]/div[1]/div[6]/button')))
+
+    time.sleep(0.4)
+
+    login_button.click()
+
+    time.sleep(5)
+
+    bot.get('https://www.instagram.com/{}/'.format(usr))
+
+    time.sleep(3.5)
+
+    WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '//*[@id="react-root"]/section/main/div/ul/li[2]/a'))).click()
+
+    time.sleep(2)
+
+    print('[Info] - Scraping...')
+
+    users = set()
+
+    for _ in range(round(user_input // 10)):
+
+        ActionChains(bot).send_keys(Keys.END).perform()
+
+        time.sleep(2)
+
+        followers = bot.find_elements_by_xpath(
+            '//*[@id="react-root"]/section/main/div/ul/div/li/div/div[1]/div[2]/div[1]/a')
+
+        # Getting url from href attribute
+        for i in followers:
+            if i.get_attribute('href'):
+                users.add(i.get_attribute('href').split("/")[3])
+            else:
+                continue
+
+    print('[Info] - Saving...')
+    print('[DONE] - Your followers are saved in followers.txt file!')
+
+    with open('followers.txt', 'a') as file:
+        file.write('\n'.join(users) + "\n")
+
+
+if __name__ == '__main__':
+    banner()
+    scrape()
